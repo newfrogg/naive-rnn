edc3e760b4cddcebaba5af15dee639944ec2f5c1
newfrogg <85973867+newfrogg@users.noreply.github.com>
2023-09-13 21:43:48 +0700

    feature complete!

---
+++ activation.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Sigmoid: 
+    def forward(self, x):
+        return 1.0 / (1.0 + np.exp(-x))
+
+    def backward(self, x, top_diff):
+        output = self.forward();
+        return (1.0 - output) * output * top_diff
+
+class Tanh:
+    def forward(self, x):
+        return np.tanh(x)
+
+    def backward(self, x, top_diff):
+        output = self.forward(x)
+        return (1.0 - np.square(output)) * top_diff
+
+class ReLu:
+    def forward(self, x):
+        return np.maximum(x, 0)
+
+
+sigmoid = Sigmoid() 
+
+x = 4
+
+print(sigmoid.forward(x));
