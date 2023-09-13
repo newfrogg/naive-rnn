082d029135e14fb5bca111a98e15c54c17a5aa94
newfrogg <85973867+newfrogg@users.noreply.github.com>
2023-09-13 21:57:05 +0700

    feature complete!

---
+++ gate.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class MultiplyGate:
+    def forward(self, W, x):
+        return np.dot(W, x)
+
+    def backward(self, W, x, dz):
+        dW = np.asarray(np.dot(np.transpose(np.asmatrix(dz)), np.asmatrix(x)))
+        dx = np.dot(np.transpose(W), dz)
+        return dW, dx
+
+class AddGate:
+    def forward(self, x1, x2):
+        return x1 + x2
+
+    def backward(self, x1, x2, dz):
+        dx1 = dz * np.ones_like(x1)
+        dx2 = dz * np.ones_like(x2)
+        return x1 * x2
+
+
+
+W = np.array([1,2,3,4])
+x = np.array([1,3,3,1])
+
+gate = MultiplyGate()
+
+print(gate.forward(W, x))
